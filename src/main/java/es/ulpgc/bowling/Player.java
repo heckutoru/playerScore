d40c2abe6a6f7311a89a53f0b1b88e3c0ec220bf
heckutoru <heckutoru@gmail.com>
2018-10-31 09:38:10 +0000

    change of repository

--- src/main/java/es/ulpgc/bowling/Player.java
+++ src/main/java/es/ulpgc/bowling/Player.java
@@ -1,4 +1,17 @@
 package es.ulpgc.bowling;
 
 public class Player {
+
+    private final PlayerScore playerS;
+
+    private final String name;
+    public Player(String name) {
+        this.name = name;
+        playerS = new PlayerScore(name);
+    }
+
+    public String getPlayerName() {
+        return name;
+    }
+
 }
--- src/test/java/es/ulpgc/bowling/PlayerTest.java
+++ src/test/java/es/ulpgc/bowling/PlayerTest.java
@@ -1,4 +1,29 @@
 package es.ulpgc.bowling;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class PlayerTest {
+
+    private Player jhon;
+    private Player jen;
+    private Player bob;
+
+    @Before
+    public void setUp() throws Exception {
+        jhon = Player("Jhon");
+        jen = Player("Jen");
+        bob = Player("Bob");
+    }
+
+    @Test
+    public void name() {
+        assertThat(jhon.getPlayerName().equals("Jhon"));
+        assertThat(jen.getPlayerName().equals("Jen"));
+        assertThat(bob.getPlayerName().equals("Bob"));
+    }
+
 }
